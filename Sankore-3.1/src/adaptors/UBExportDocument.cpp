67e49c34fa999f794cea28c198c7b6c149f4391e
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2013-05-05 13:22:15 +0200

    Imported Upstream version 2.1.0+git20130505

--- Sankore-3.1/src/adaptors/UBExportDocument.cpp
+++ Sankore-3.1/src/adaptors/UBExportDocument.cpp
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2012 Webdoc SA
+ * Copyright (C) 2010-2013 Groupement d'Intérêt Public pour l'Education Numérique en Afrique (GIP ENA)
  *
  * This file is part of Open-Sankoré.
  *
  * Open-Sankoré is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, version 3 of the License,
  * with a specific linking exception for the OpenSSL project's
  * "OpenSSL" library (or with modified versions of it that use the
  * same license as the "OpenSSL" library).
@@ -28,6 +28,7 @@
 #include "core/UBApplication.h"
 
 #include "document/UBDocumentProxy.h"
+#include "document/UBDocumentController.h"
 
 #include "globals/UBGlobals.h"
 
@@ -125,3 +126,14 @@ QString UBExportDocument::exportName()
 {
     return tr("Export to Sankore Format");
 }
+
+bool UBExportDocument::associatedActionactionAvailableFor(const QModelIndex &selectedIndex)
+{
+    const UBDocumentTreeModel *docModel = qobject_cast<const UBDocumentTreeModel*>(selectedIndex.model());
+    if (!selectedIndex.isValid() || docModel->isCatalog(selectedIndex)) {
+        return false;
+    }
+
+    return true;
+}
+
